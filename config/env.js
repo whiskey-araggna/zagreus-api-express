2b1ebce9966b1df2bc17747df9156cbe731ad794
widya.anggara <widya.anggara@inteleq.id>
2025-05-26 16:45:28 +0700

    fix for Railway deploy

--- config/env.js
+++ config/env.js
@@ -23,3 +23,11 @@ function loadEnv() {
 }
 
 loadEnv();
+
+export const envConfig = {
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+};
